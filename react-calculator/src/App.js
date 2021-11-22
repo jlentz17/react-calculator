e5c147153f170f4383fd6be7ea3e0e22235631b4
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-11-21 21:26:11 -0700

    start

---
+++ react-calculator/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'></div>
+        <div className='current-operand'></div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+    </div>
+  );
+}
+
+export default App;
